be56abba92382de536c7686ccfce8a82dcc1665d
WojciechSova <dzidadzidowski@gmail.com>
2021-03-06 19:34:51 +0100

    Add app and app using library

---
+++ app/src/main/java/pl/java/zzpj/App.java
@@ -0,0 +1,15 @@
+package pl.java.zzpj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Person person = new Person();
+        person.setName("Robert");
+        person.hello();
+    }
+}
