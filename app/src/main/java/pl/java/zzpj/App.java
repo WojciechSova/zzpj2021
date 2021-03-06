2e9addab8d4ecbe50fd30da328281a9ae050b832
WojciechSova <dzidadzidowski@gmail.com>
2021-03-06 20:16:27 +0100

    Add properties

--- app/src/main/java/pl/java/zzpj/App.java
+++ app/src/main/java/pl/java/zzpj/App.java
@@ -1,5 +1,9 @@
 package pl.java.zzpj;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 /**
  * Hello world!
  *
@@ -9,7 +13,18 @@ public class App
     public static void main( String[] args )
     {
         Person person = new Person();
-        person.setName("Robert");
+        person.setName(getClientNameProperty());
         person.hello();
+    }
+
+    public static String getClientNameProperty() {
+        Properties prop = new Properties();
+        try (InputStream resourceAsStream = App.class.getClassLoader().getResourceAsStream("config.properties")) {
+            prop.load(resourceAsStream);
+        } catch (IOException e) {
+            System.err.println("Unable to load properties file\n");
+            e.printStackTrace();
+        }
+        return prop.getProperty("personName");
     }
 }
